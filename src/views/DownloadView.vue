ec7f738ccd45c18cdde927d1c8cea208ba4fe806
FurWaz <fur.waz06@gmail.com>
2024-09-06 00:24:33 +0200

    Modified more links to furwaz.com

--- src/views/DownloadView.vue
+++ src/views/DownloadView.vue
@@ -63,7 +63,7 @@ export default defineComponent({
                     name: 'Windows', 
                     viewbox: '0 0 448 512', 
                     path: "M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6 25.3V268.4H0v149.9zm203.8 28L448 480V268.4H203.8v177.9zm0-380.6v180.1H448V32L203.8 65.7z", 
-                    link: "https://cdn.furwaz.fr/fullbowody/FullBowody-0.1.0-Setup.exe" 
+                    link: "https://cdn.furwaz.com/fullbowody/FullBowody-0.1.0-Setup.exe" 
                 }, 
                 { 
                     platform: 'macos', 
